6ab42ce6f9d32edf6f6c003f2ad3d034759a4fd5
Onur MORCAGÖK <55251990+onurmorcagok@users.noreply.github.com>
2020-10-07 22:24:22 +0300

    Update TranslateForm.vue

--- vue-translator/src/components/TranslateForm.vue
+++ vue-translator/src/components/TranslateForm.vue
@@ -33,19 +33,19 @@ export default {
       translateTo: "",
     };
   },
-    methods: {
-      translateIt() {
-          let baseURL =
-            "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20170329T180255Z.3c18d2dc7b65d525.f23ed9a9efa992bded4ef96334e3c154f61d2dea&lang=en'+language+'&text="
-            this.translateText;
-          axios
-            .get(baseURL)
-            .then((response) => {
-              console.log(response);
-            })
-            .catch((e) => console.log(e));
-      },
+  methods: {
+    translateIt() {
+      let baseURL =
+        "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20181116T200129Z.775558096056e445.a0cb98993d244e1a93e7aa435b7a0f60a6b5ab4d&text=";
+      this.translateText + "&lang=en";
+      axios
+        .get(baseURL)
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((e) => console.log(e));
     },
+  },
 };
 </script>
 
